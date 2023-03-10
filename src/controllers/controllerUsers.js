59a747033f532ff816114632a7c35353e88b6bd3
unknown <juan.figueroa01@uptc.edu.co>
2023-03-10 17:21:24 -0500

    borrar y editar usuario

--- src/controllers/controllerUsers.js
+++ src/controllers/controllerUsers.js
@@ -1,23 +1,23 @@
-const {PrismaClient} = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 const { encrypt, compare } = require('../utils/handleBcrypt')
 const jwt = require('jsonwebtoken')
 
 
-function parseJwt (token) {
+function parseJwt(token) {
     return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
 }
 
-const getUsers = async (req, res) => { 
+const getUsers = async (req, res) => {
     try {
         const data = await prisma.users.findMany({
-        })        
-        if (data[0] === undefined){
+        })
+        if (data[0] === undefined) {
             res.status(400).send({
                 message: "No se encuentra los datos solicitados"
             })
-        }else{
-            res.json(data) 
+        } else {
+            res.json(data)
         }
     } catch (error) {
         res.send({
@@ -27,24 +27,47 @@ const getUsers = async (req, res) => {
     }
 }
 
-const getUserById = async (req, res) => { 
+const getUserById = async (req, res) => {
     try {
         const data = await prisma.users.findUnique({
-            where:{
+            where: {
                 user_id: req.body.user_id
             }
-        })        
+        })
         console.log(data);
-        if (data === null){
+        if (data === null) {
             res.status(400).send({
                 message: "No se encuentra los datos solicitados"
             })
-        }else{
-            res.json(data) 
+        } else {
+            res.json(data)
         }
     } catch (error) {
         res.send({
             message: "Ocurrió un error al momento obtener los usuarios"
+        })
+        console.log(error)
+    }
+}
+
+const getUserByState = async (req, res) => {
+    try {
+        const data = await prisma.users.findUnique({
+            where: {
+                user_state: req.body.user_state
+            }
+        })
+        console.log(data);
+        if (data === null) {
+            res.status(400).send({
+                message: "No se encuentra los datos solicitados"
+            })
+        } else {
+            res.json(data)
+        }
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento obtener los usuarios por estado"
         })
         console.log(error)
     }
@@ -52,18 +75,17 @@ const getUserById = async (req, res) => {
 
 const createUsers = async (req, res) => {
     try {
-        const product =await prisma.users.create({
-            data:{
+        const user = await prisma.users.create({
+            data: {
                 first_name: req.body.first_name,
                 last_name: req.body.last_name,
                 email: req.body.email,
                 user_password: await encrypt(req.body.user_password),
                 user_state: req.body.user_state,
                 manager_id: req.body.manager_id
-
             }
         })
-        
+
         res.send({
             message: "Usuario creado con éxito"
         });
@@ -76,10 +98,70 @@ const createUsers = async (req, res) => {
 
 }
 
-const getUser = (req, res) => { }
 
-const updateUsers = (req, res) => { }
+const updateUsers = async (req, res) => {
 
-const deleteUsers = (req, res) => { }
+    try {
+        const user = await prisma.users.update({
+            where: {
+                user_id: req.body.user_id
+            },
+            data: {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                user_password: req.body.user_password,
+                user_state: req.body.user_state,
+                manager_id: req.body.manager_id
+            }
+        })
+        createAudit(req, res, "Se actualizo la persona con el id " + user.user_id)
+        res.send({
+            message: "Persona actualizada con éxito."
+        });
+    } catch (error) {
+        if (error.code === undefined) {
+            res.status(400).send({
+                message: "Ocurrió un error al actualizar al usuario"
+            })
+        } else {
+            res.status(400).send({
+                message: "Ocurrió un error al actualizar al usuario"
+            })
+        }
+        console.log(error)
+    }
 
-module.exports = { getUsers, createUsers, getUser,getUserById,  updateUsers, deleteUsers }
+
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await prisma.users.update({
+            where: {
+                user_id: req.body.user_id
+            },
+            data: {
+                user_state: "I",
+            }
+        })
+        createAudit(req, res, "Se eliminó la persona con el id " + user.user_id)
+        res.send({
+            message: "Persona borrada con éxito."
+        });
+    } catch (error) {
+        if (error.code === undefined) {
+            res.status(400).send({
+                message: "Ocurrió un error al eliminar el usuario"
+            })
+        } else {
+            res.status(400).send({
+                message: "Ocurrió un error al eliminar el usuario"
+            })
+        }
+        console.log(error)
+    }
+
+}
+
+module.exports = { getUsers, createUsers, getUserById, updateUsers, deleteUser, getUserByState }
--- src/routes/users/routeUsers.js
+++ src/routes/users/routeUsers.js
@@ -1,16 +1,16 @@
 const router = require('express').Router()
-const {getUsers, createUsers, getUserById} = require('../../controllers/controllerUsers') 
+const {getUsers, createUsers, getUserById, updateUsers, deleteUser, getUserByState} = require('../../controllers/controllerUsers') 
 
 router.get('/getUsers', getUsers)
 
-router.get('/getUserByState', )
+router.get('/getUserByState', getUserByState)
 
 router.get('/getUserById', getUserById)
 
 router.post('/createUser', createUsers)  
 
-router.put('/updateUser')
+router.put('/updateUser',updateUsers)
 
-router.put('/deleteUser')
+router.put('/deleteUser', deleteUser)
 
 module.exports = router
