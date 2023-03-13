f252fef31463031a75d8dcfdab1a7f1400932465
unknown <juan.figueroa01@uptc.edu.co>
2023-03-12 19:26:49 -0500

    creacion y lectura inicial del registro de tareas

---
+++ src/controllers/controllerHistoryTasks.js
@@ -0,0 +1,123 @@
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+const jwt = require('jsonwebtoken')
+
+const getHisTasksByUser = async (req, res) => {
+    try {
+        const data = await prisma.history_tasks.findMany({
+            where: {
+                user_id: req.body.user_id
+            }
+        })
+        if (data[0] === undefined) {
+            res.status(400).send({
+                message: "No se encuentra los datos solicitados"
+            })
+        } else {
+            res.json(data)
+        }
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento obtener las tareas del usuario"
+        })
+        console.log(error)
+    }
+}
+
+const getHistoryTasks = async (req, res) => {
+    try {
+        const data = await prisma.history_tasks.findMany({
+        })
+        if (data[0] === undefined) {
+            res.status(400).send({
+                message: "No se encuentra los datos solicitados"
+            })
+        } else {
+            res.json(data)
+        }
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento obtener todo el historial de las tareas"
+        })
+        console.log(error)
+    }
+}
+
+const assignHistoryTask = async (req, res) => {
+    try {
+        const data = await prisma.history_tasks.create({
+            data: {
+                user_id: req.body.user_id,
+                task_id: req.body.task_id,
+                state_id: req.body.state_id,
+                change_date: new Date(req.body.change_date)
+            }
+        })
+
+        res.send({
+            message: "tarea asinganada con éxito"
+        });
+    } catch (error) {
+        res.status(400).send({
+            message: "Ocurrió el error al asignar una tarea"
+        });
+        console.log(error)
+    }
+}
+
+const registerHistoryTask = async (req, res) => {
+    try {
+        const data = await prisma.history_tasks.create({
+            data: {
+                user_id: req.body.user_id,
+                task_id: req.body.task_id,
+                state_id: req.body.state_id,
+                change_date: new Date(req.body.change_date)
+            }
+        })
+
+        res.send({
+            message: "historial de tarea creada con éxito"
+        });
+    } catch (error) {
+        res.status(400).send({
+            message: "Ocurrió el error al momento de crear una registro en el historial de tarea"
+        });
+        console.log(error)
+    }
+}
+
+//TO-DO
+const getLastPositionTask = async (req, res) => {
+    try {
+        const data = await prisma.history_tasks.findMany({
+            where: {
+                user_id: req.body.task_id,
+                task_id: req.body.task_id
+            },
+            orderBy: {
+                change_date: 'desc'
+            }
+        })
+        if (data === null) {
+            res.status(400).send({
+                message: "No se encuentra los datos solicitados"
+            })
+        } else {
+            res.json(data[0])
+        }
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento obtener las tareas"
+        })
+        console.log(error)
+    }
+}
+
+
+
+
+
+module.exports = { getHisTasksByUser, getLastPositionTask,
+                    getHisTasksByUser, getHistoryTasks, assignHistoryTask,
+                     registerHistoryTask }
--- src/routes/users/routeHistoryTasks.js
+++ src/routes/users/routeHistoryTasks.js
@@ -1,6 +1,16 @@
+const { getLastPositionTask, assignHistoryTask, getHisTasksByUser, registerHistoryTask, getHistoryTasks } = require('../../controllers/controllerHistoryTasks')
+
 const router = require('express').Router()
 
-router.get('/getHistoryTasks')
-router.post('/registerHistoryTask')
+router.get('/getHistoryTasks', getHistoryTasks)
+
+router.post('/registerHistoryTask', registerHistoryTask)
+
+router.post('/assignHistoryTask', assignHistoryTask)
+
+router.get('/getHisTasksByUser', getHisTasksByUser)
+
+router.get('/getLastPositionTask', getLastPositionTask)
+
 
 module.exports = router
